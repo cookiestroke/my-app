217f558f0d6156edb806be5876ec67c5d109fc20
Mandar Mhaske <mmhaske@jstcap.com>
2023-10-30 17:25:27 -0400

    update

--- src/lib/Sidebar.js
+++ src/lib/Sidebar.js
@@ -1,27 +1,29 @@
-import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import React, { useState } from "react";
+
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import { NavData } from "../lib/NavData";
 import { NavLink } from "react-router-dom";
-import styles from "../components/Sidebar.css"
-import { useState } from "react";
+import styles from "../components/Sidebar.module.css"; // Update the import for CSS Modules
 
 export default function Sidebar() {
-    const [open, setopen] = useState(true)
-    const toggleOpen = () => {
-        setopen(!open)
-    }
-  return (
-    <div className={open?styles.sidebar:styles.sidebarClosed}>
-        <button className={styles.menuBtn} onClick={toggleOpen}>
-            {open? <KeyboardDoubleArrowLeftIcon />: <KeyboardDoubleArrowRightIcon />}
-        </button>
-        {NavData.map(item =>{
-            return <NavLink key={item.id} className={styles.sideitem} to={item.link}>
-            {item.icon}
-            <span className={styles.linkText}>{item.text}</span>
-        </NavLink>
-        })}
-    </div>
-  )
-}
+  const [open, setOpen] = useState(true);
 
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className={open ? styles.sidebar : styles.sidebarClosed}>
+      <button className={styles.menuBtn} onClick={toggleOpen}>
+        {open ? <KeyboardDoubleArrowLeftIcon /> : <KeyboardDoubleArrowRightIcon />}
+      </button>
+      {NavData.map((item) => (
+        <NavLink key={item.id} className={styles.sideitem} to={item.link}>
+          {item.icon}
+          <span className={styles.linkText}>{item.text}</span>
+        </NavLink>
+      ))}
+    </div>
+  );
+}
