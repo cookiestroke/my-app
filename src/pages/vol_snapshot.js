1bf1d9c2fcede105ac28d8cbc4171633312a7c8c
Mandar Mhaske <mmhaske@jstcap.com>
2023-10-31 17:47:23 -0400

    update

---
+++ src/pages/vol_snapshot.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+
+import DataTable from './DataTable';
+
+const Day30Vol = () => {
+	const [data, setData] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [columns, setColumns] = useState([]);
+  	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		// Fetch the data from the data.json file
+		fetch('/data_table_big.json')
+			.then((response) => response.json())
+			.then((jsonData) => {
+				setData(jsonData);
+				// Extract column definitions from the first item (assuming all items have the same structure)
+				if (jsonData.length > 0) {
+					const firstItem = jsonData[0];
+					const extractedColumns = Object.keys(firstItem).map((key) => ({
+					  header: key,
+					  dataField: key,
+					}));
+					setColumns(extractedColumns);
+				  }
+		  
+				setLoading(false);
+			})
+			.catch((error) => {
+				console.error('Error fetching data:', error);
+				setLoading(false);
+			});
+	}, []);
+
+	// Calculate the total number of pages based on the data length
+	const totalPages = Math.ceil(data.length / 10);
+
+	const handlePageChange = (newPage) => {
+		setCurrentPage(newPage);
+	};
+
+	return (
+		<div>
+			<h2 style={{ textAlign: 'center' }}>L30D Volume by Day (JST)</h2>
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+			<DataTable
+				data={data}
+				columns={columns}
+				currentPage={currentPage}
+				totalPages={totalPages}
+				onPageChange={handlePageChange}
+			/>
+			)}
+		</div>
+	);
+};
+
+export default Day30Vol;
--- src/pages/volume.js
+++ src/pages/volume.js
@@ -2,29 +2,33 @@ import Day30Vol from './day_30_vol';
 import JSTVolume from './jst_volume';
 import MKTVolume from './mkt_volume';
 import React from 'react';
+import VolSnapshot from './vol_snapshot';
 
 function Volume() {
-  const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column', // Stack the children vertically
-  };
+	const containerStyle = {
+		display: 'flex',
+		flexDirection: 'column', // Stack the children vertically
+	};
 
-  const rowStyle = {
-    display: 'flex',
-    justifyContent: 'space-between', // Put the two volumes side by side
-  };
+	const rowStyle = {
+		display: 'flex',
+		justifyContent: 'space-between', // Put the two volumes side by side
+	};
 
-  return (
-    <div style={containerStyle}>
-      <div style={rowStyle}>
-        <JSTVolume />
-        <MKTVolume />
-      </div>
-      <div>
-        <Day30Vol />
-      </div>
-    </div>
-  );
+	return (
+		<div style={containerStyle}>
+			<div>
+				<VolSnapshot />
+			</div>
+			<div style={rowStyle}>
+				<JSTVolume />
+				<MKTVolume />
+			</div>
+			<div>
+				<Day30Vol />
+			</div>
+		</div>
+	);
 }
 
 export default Volume;
