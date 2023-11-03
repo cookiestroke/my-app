22393e5349b3231334f7a4309fd41bec085dec62
Mandar Mhaske <mmhaske@jstcap.com>
2023-11-03 16:50:39 -0400

    update

--- src/TradeApp.js
+++ src/TradeApp.js
@@ -2,30 +2,30 @@ import './components/TradeApp.css';
 
 import React, { useState } from 'react';
 import {
-  Route,
-  Routes,
+	Route,
+	Routes,
 } from "react-router-dom";
 
 import Home from "./pages/home";
-import Sidebar from './lib/Sidebar'; // Adjust the import if necessary
+import Sidebar from './lib/Sidebar';
 import Spread from "./pages/spread";
 import Volume from "./pages/volume";
 
-function App() {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
+function TradeApp() {
+	const [isSidebarOpen, setSidebarOpen] = useState(true);
 
-  return (
-    <div className="App">
-      <Sidebar open={isSidebarOpen} toggleOpen={() => setSidebarOpen(!isSidebarOpen)} />
-      <main className={isSidebarOpen ? "mainOpen" : "mainClosed"}>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/spread" element={<Spread />} />
-          <Route path="/volume" element={<Volume />}/>
-        </Routes>
-      </main>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Sidebar open={isSidebarOpen} toggleOpen={() => setSidebarOpen(!isSidebarOpen)} />
+			<main className={`mainContent ${isSidebarOpen ? "mainOpen" : "mainClosed"}`}>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/spread" element={<Spread />} />
+					<Route path="/volume" element={<Volume />} />
+				</Routes>
+			</main>
+		</div>
+	);
 }
 
-export default App;
+export default TradeApp;
