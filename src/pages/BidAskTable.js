f88999ca86abb5b38927b2a71f91f3a8f09056cb
Mandar Mhaske <mmhaske@jstcap.com>
2023-11-08 09:29:03 -0800

    update

--- src/pages/BidAskTable.js
+++ src/pages/BidAskTable.js
@@ -1,37 +1,57 @@
+import '../components/DataTable.css';
+
 import React, { useEffect, useState } from 'react';
 
-// Assuming DataTable is a custom component or from another library
 import DataTable from './DataTable';
 
+// Helper function to calculate average depths from the JSON data
+function calculateAverageDepths(data) {
+	const bids = {};
+	const asks = {};
+	for (const [coin, coinData] of Object.entries(data)) {
+		bids[coin] = {};
+		asks[coin] = {};
+		for (const [bps, depthArray] of Object.entries(coinData.bids)) {
+			bids[coin][bps] = depthArray.reduce((acc, { depth }) => acc + depth, 0) / depthArray.length;
+		}
+		for (const [bps, depthArray] of Object.entries(coinData.asks)) {
+			asks[coin][bps] = depthArray.reduce((acc, { depth }) => acc + depth, 0) / depthArray.length;
+		}
+	}
+	return { bids, asks };
+}
+
+// Helper function to calculate the top of book averages
+function calculateTopOfBookAverages(bidsData, asksData) {
+	// Assuming '10bps' is the top of the book level
+	const averages = { 'Average Top of Book': 'Avg' };
+	Object.keys(bidsData).forEach((coin) => {
+		const bid = bidsData[coin]['10bps'];
+		const ask = asksData[coin]['10bps'];
+		averages[coin] = ((bid + ask) / 2).toFixed(3);
+	});
+	return averages;
+}
+
 const DepthTable = ({ data, title }) => {
-    // Convert data to the array format expected by DataTable
-    const tableData = Object.entries(data).map(([coin, depthLevels]) => ({
-        coin,
-        ...depthLevels,
-    }));
+	const tableData = Object.entries(data).map(([coin, depthLevels]) => ({
+		coin,
+		...depthLevels,
+	}));
 
-    // Generate columns dynamically from the first item's keys
-    const columns = tableData.length > 0
-        ? Object.keys(tableData[0]).map(key => ({
-              header: key,
-              dataField: key,
-          }))
-        : [];
+	const columns = Object.keys(tableData[0]).map(key => ({
+		header: key === 'coin' ? title : key,
+		dataField: key,
+	}));
 
-    // Debug log to check the structure of tableData and columns
-    console.log('Table Data:', tableData);
-    console.log('Columns:', columns);
-
-    return (
-        <DataTable
-            data={tableData}
-            columns={columns}
-        />
-    );
+	return (
+		<DataTable
+			data={tableData}
+			columns={columns}
+		/>
+	);
 };
 
-
-// Main component that renders the two tables
 const BidAskTable = () => {
 	const [bidsData, setBidsData] = useState({});
 	const [asksData, setAsksData] = useState({});
@@ -55,40 +75,35 @@ const BidAskTable = () => {
 		fetchData();
 	}, []);
 
+	const topOfBookAverages = calculateTopOfBookAverages(bidsData, asksData);
+	const averageRow = [topOfBookAverages];
+	const averageColumns = Object.keys(topOfBookAverages).map(key => ({
+		header: key,
+		dataField: key,
+	}));
+
 	if (loading) {
 		return <div>Loading...</div>;
 	}
 
 	return (
-		<div className="row">
-			<div>
-				<h3>Bids</h3>
-				<DepthTable data={bidsData} title="Bids" />
+		<div>
+			<div className="table-container">
+				<h3>Average Top of Book Per Coin</h3>
+				<DataTable data={averageRow} columns={averageColumns} />
 			</div>
-			<div>
-				<h3>Asks</h3>
-				<DepthTable data={asksData} title="Asks" />
+			<div className="row">
+				<div className="table-container">
+					<h3>Bids</h3>
+					<DepthTable data={bidsData} title="Bids" />
+				</div>
+				<div className="table-container">
+					<h3>Asks</h3>
+					<DepthTable data={asksData} title="Asks" />
+				</div>
 			</div>
 		</div>
 	);
 };
 
 export default BidAskTable;
-
-// Helper function to calculate average depths from the JSON data
-function calculateAverageDepths(data) {
-	// Assuming 'data' is the parsed JSON object from '/bps_data.json'
-	const bids = {};
-	const asks = {};
-	for (const [coin, coinData] of Object.entries(data)) {
-		bids[coin] = {};
-		asks[coin] = {};
-		for (const [bps, depthArray] of Object.entries(coinData.bids)) {
-			bids[coin][bps] = depthArray.reduce((acc, { depth }) => acc + depth, 0) / depthArray.length;
-		}
-		for (const [bps, depthArray] of Object.entries(coinData.asks)) {
-			asks[coin][bps] = depthArray.reduce((acc, { depth }) => acc + depth, 0) / depthArray.length;
-		}
-	}
-	return { bids, asks };
-}
